2a5fdfaf9195019cb81a356e7bcab17fa88b8e0e
jitesh77 <jitesh.gupta77@gmail.com>
2018-09-18 00:36:19 +0530

    changes

--- HR/email.php
+++ HR/email.php
@@ -1,5 +1,5 @@
 <?php
-$to = "sbpl.infra@gmail.com";
+$to = "jitesh.gupta1909@gmail.com";
 
 $subject = "Message from SBPL";
 
@@ -7,11 +7,11 @@ $headers = 'MIME-Version: 1.0' . "\r\n";
 
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-$headers = "From: khandelwalsurendra9@gmail.com\r\n";
+$headers = "From: jitesh.gupta77@gmail.com\r\n";
 
-$headers .= "Reply-To: khandelwalsurendra9@gmail.com\r\n";
+$headers .= "Reply-To: jitesh.gupta77@gmail.com\r\n";
 
-$headers .= "Return-Path: khandelwalsurendra9@gmail.com\r\n";
+$headers .= "Return-Path: jitesh.gupta77@gmail.com\r\n";
 
 $text ="Hi ".$_REQUEST['name'].","."\r\n" .
 
@@ -22,7 +22,7 @@ $text ="Hi ".$_REQUEST['name'].","."\r\n" .
 "<br>Sender Name: ".$_REQUEST['name']." <br> Sender Email:".$_REQUEST['email']." <br> Sender Subject:".$_REQUEST['subject']."<br>Message: ".$_REQUEST['message']."";
 $txt = wordwrap($text, 70);
 
-if (mail($to,$subject,$txt,$headers, '-freturn@digiwage.org')) {
+if (mail($to,$subject,$txt,$headers, '-freturn@sbplinfra.com')) {
     echo 1;
 
 } else {
