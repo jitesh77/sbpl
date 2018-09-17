50875fe2ba5495115f8ececeff0a522b24cc4bf3
jitesh77 <jitesh.gupta77@gmail.com>
2018-09-17 15:34:54 +0530

    changes

---
+++ HR/email.php
@@ -0,0 +1,33 @@
+<?php
+$to = "sbpl.infra@gmail.com";
+
+$subject = "Message from SBPL";
+
+$headers = 'MIME-Version: 1.0' . "\r\n";
+
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+$headers = "From: khandelwalsurendra9@gmail.com\r\n";
+
+$headers .= "Reply-To: khandelwalsurendra9@gmail.com\r\n";
+
+$headers .= "Return-Path: khandelwalsurendra9@gmail.com\r\n";
+
+$text ="Hi ".$_REQUEST['name'].","."\r\n" .
+
+"You have a received message from SBPL"."\r\n" .
+
+"".$_REQUEST['message']."\r\n" .
+
+"<br>Sender Name: ".$_REQUEST['name']." <br> Sender Email:".$_REQUEST['email']." <br> Sender Subject:".$_REQUEST['subject']."<br>Message: ".$_REQUEST['message']."";
+$txt = wordwrap($text, 70);
+
+if (mail($to,$subject,$txt,$headers, '-freturn@digiwage.org')) {
+    echo 1;
+
+} else {
+    echo 0;
+    
+}
+?>
+
